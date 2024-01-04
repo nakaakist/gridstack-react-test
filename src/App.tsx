2686e2bd733f8e30c8877fc4cc0413075736f6fa
Nariyuki Saito <toshincompos@gmail.com>
2024-01-04 23:57:19 +0900

    Add first grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { GridStack } from "gridstack";
+import "gridstack/dist/gridstack.css";
+import { useEffect } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  useEffect(() => {
+    GridStack.init();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="grid-stack">
+        <div className="grid-stack-item" data-gs-width="4" data-gs-height="4">
+          <div className="grid-stack-item-content">Item 1</div>
+        </div>
+        <div className="grid-stack-item" data-gs-width="4" data-gs-height="4">
+          <div className="grid-stack-item-content">Item 2</div>
+        </div>
+        <div className="grid-stack-item" data-gs-width="4" data-gs-height="4">
+          <div className="grid-stack-item-content">Item 3</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    </div>
+  );
+};
